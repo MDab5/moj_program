efc552181d8867a9b33175fe84fba6c75ad3eae9
Mateusz <m.dabrowski.510@studms.ug.edu.pl>
2025-11-14 23:23:03 +0100

    Program kalkulator

---
+++ kalkulator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+ int a = 10;
+ int b = 5;
+ int dodawanie = a+b;
+ int odejmowanie = a-b;
+ int mnozenie = a*b;
+ int dzielenie = a/b;
+ printf("Wynik dodawania zmiennych a i b daje wynik: %d", dodawanie);
+ printf("\nWynik odejmowania zmiennych a i b daje wynik: %d", odejmowanie);
+ printf("\nWynik mnozenia zmiennych a i b daje wynik: %d", mnozenie);
+ printf("\nWynik dzielenia zmiennych a i b daje wynik: %d", dzielenie);
+
+ return 0;
+}
