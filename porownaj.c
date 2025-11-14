ee4f72919f07b0bc9414f046a47c8ebead14196f
Mateusz <m.dabrowski.510@studms.ug.edu.pl>
2025-11-15 00:23:48 +0100

    Program porownujacy liczby

---
+++ porownaj.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+ int a = 10;
+ int b = 5;
+
+ if(a > b) printf("%d", a);
+ if(b > a) printf("%d", b);
+ if(a == b) printf("Zmienne sa rowne");
+
+ return 0;
+
+}
